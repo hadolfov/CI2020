b368591a6a43e273bd0e4e1bd3c0e745a47279c2
greivin_p <greivin_p@hotmail.com>
2020-04-02 00:02:28 -0600

    Obj Mantenimiento

---
+++ HappyPet4.0 - Greivin/DL/SCH_MANTENIMIENTO/Caja.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DL.SCH_MANTENIMIENTO
+{
+    public class Caja
+    {
+		private int _idcaja;
+		private string desCaja;
+		private int _idSucursal;
+		private int _estado;
+
+		public int Idcaja { get => _idcaja; set => _idcaja = value; }
+		public string DesCaja { get => desCaja; set => desCaja = value; }
+		public int IdSucursal { get => _idSucursal; set => _idSucursal = value; }
+		public int Estado { get => _estado; set => _estado = value; }
+	}
+	/*
+     [IdCaja] [int] IDENTITY(1,1) NOT NULL,
+	[desCaja] [nvarchar](100) NULL,
+	[IdSucursal] [int] NULL,
+	[IdUsuarioSeguridad] [int] NULL,
+	[UsuarioCreacion] [int] NULL,
+	[FechaCreacion] [datetime] NULL,
+	[UsuarioModificacion] [int] NULL,
+	[FechaModificacion] [datetime] NULL,
+	[IdEstado] [int] NULL,
+     */
+}
---
+++ HappyPet4.0 - Greivin/DL/SCH_MANTENIMIENTO/DiaSemana.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DL.SCH_MANTENIMIENTO
+{
+    public class DiaSemana
+    {
+        private int _idDia;
+        private string _dia;
+
+        public int IdDia { get => _idDia; set => _idDia = value; }
+        public string Dia { get => _dia; set => _dia = value; }
+    }
+}
